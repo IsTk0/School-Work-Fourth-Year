3eadd4e9e0d81c3dbd3e9f5926f20af5ff9be80d
Caffè - Antu <97669353+IsTk0@users.noreply.github.com>
2023-11-13 19:53:41 +0100

    Add files via upload

--- Java/L'eredita immobiliare/Main.java
+++ Java/L'eredita immobiliare/Main.java
@@ -7,57 +7,67 @@ public class Main {
 
         Abitazione[] abitazioni = new Abitazione[2];
 
+        int numeroDiPiani;
+        int superficie;
+        int superficieGiardino;
+        int piano;
+        int numeroDiBalconi;
+        String indirizzo;
+        String tipoAbitazione;
+        boolean piscina;
+        boolean ascensore;
+
         // Ciclo per l'inserimento automatico dei dati
         for (int i = 0; i < abitazioni.length; i++) {
             System.out.println("Inserisci i dati per l'abitazione " + (i + 1));
 
             do{
                 System.out.print("Numero di piani: ");
-                int numeroDiPiani = input.nextInt();
+                numeroDiPiani = input.nextInt();
             }while(numeroDiPiani <= 0);
             
             do{
                 System.out.print("Superficie: ");
-                int superficie = input.nextInt();
+                superficie = input.nextInt();
             }while(superficie <= 0);
 
             do{
                 System.out.print("Indirizzo: ");
-                String indirizzo = input.nextLine();
+                indirizzo = input.nextLine();
             }while(indirizzo.length() <= 0);
 
             do{
                 System.out.print("Tipo di abitazione (Villa o Appartamento): ");
-                String tipoAbitazione = input.nextLine();
-            }while(tipoAbitazione != "Villa" || tipoAbitazione != "Appartamento");
+                tipoAbitazione = input.nextLine();
+            }while(tipoAbitazione.isEmpty());
 
-            if ("Villa" == tipoAbitazione) {
+            if ("Villa".equals(tipoAbitazione)) {
                 do{
                     System.out.print("Superficie giardino: ");
-                    int superficieGiardino = input.nextInt();
+                    superficieGiardino = input.nextInt();
                 }while(superficieGiardino <= 0);
                 
                 do{
                     System.out.print("Presenza piscina (true/false): ");
-                    boolean piscina = input.nextBoolean();
+                    piscina = input.nextBoolean();
                 }while(piscina != true || piscina != false);
 
                 abitazioni[i] = new Villa(numeroDiPiani, superficie, indirizzo, superficieGiardino, piscina);
 
-            } else if ("Appartamento" == tipoAbitazione) {
+            } else if ("Appartamento".equals(tipoAbitazione)) {
                 do{
                     System.out.print("Piano: ");
-                    int piano = input.nextInt();
+                    piano = input.nextInt();
                 }while(piano <= 0);
                 
                 do{
                     System.out.print("Numero di balconi: ");
-                    int numeroDiBalconi = input.nextInt();
+                    numeroDiBalconi = input.nextInt();
                 }while(numeroDiBalconi <= 0);
 
                 do{
                     System.out.print("Presenza ascensore (true/false): ");
-                    boolean ascensore = input.nextBoolean();
+                    ascensore = input.nextBoolean();
                 }while(ascensore != true || ascensore != false);
 
                 abitazioni[i] = new Appartamento(numeroDiPiani, superficie, indirizzo, piano, numeroDiBalconi, ascensore);
